a6323569849b973dc82cbdcfaa6febef697a1e51
Marcos Gronytzki <gronytzki>
2019-10-04 16:13:11 -0300

    Seila, só sei que a sorte um dia vem

--- platforms/android/app/src/main/assets/www/pages-cadastro-assistente-cadastro-assistente-module-es2015.js
+++ platforms/android/app/src/main/assets/www/pages-cadastro-assistente-cadastro-assistente-module-es2015.js
@@ -154,14 +154,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CadastroAssistentePage", function() { return CadastroAssistentePage; });
 /* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
-/* harmony import */ var src_app_services_assistenteCadastro_assistenteCadastro__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! src/app/services/assistenteCadastro/assistenteCadastro */ "./src/app/services/assistenteCadastro/assistenteCadastro.ts");
-/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm2015/router.js");
-/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
-/* harmony import */ var src_app_services_assistenteCadastro_assistente_cadastro_service__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! src/app/services/assistenteCadastro/assistente-cadastro.service */ "./src/app/services/assistenteCadastro/assistente-cadastro.service.ts");
-/* harmony import */ var _ionic_native_camera_ngx__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @ionic-native/camera/ngx */ "./node_modules/@ionic-native/camera/ngx/index.js");
-/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm2015/forms.js");
-/* harmony import */ var src_app_core_overlay_service__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! src/app/core/overlay.service */ "./src/app/core/overlay.service.ts");
-
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm2015/router.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
+/* harmony import */ var src_app_services_assistenteCadastro_assistente_cadastro_service__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! src/app/services/assistenteCadastro/assistente-cadastro.service */ "./src/app/services/assistenteCadastro/assistente-cadastro.service.ts");
+/* harmony import */ var _ionic_native_camera_ngx__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @ionic-native/camera/ngx */ "./node_modules/@ionic-native/camera/ngx/index.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm2015/forms.js");
+/* harmony import */ var src_app_core_overlay_service__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! src/app/core/overlay.service */ "./src/app/core/overlay.service.ts");
 
 
 
@@ -171,14 +169,13 @@ __webpack_require__.r(__webpack_exports__);
 
 
 let CadastroAssistentePage = class CadastroAssistentePage {
-    constructor(route, loadingController, assistenteCadastroService, navCtrl, camera, overlay, assistenteCadastro) {
+    constructor(route, loadingController, assistenteCadastroService, navCtrl, camera, overlay) {
         this.route = route;
         this.loadingController = loadingController;
         this.assistenteCadastroService = assistenteCadastroService;
         this.navCtrl = navCtrl;
         this.camera = camera;
         this.overlay = overlay;
-        this.assistenteCadastro = assistenteCadastro;
         this.todas = {
             id: '',
             apelido: '',
@@ -261,17 +258,16 @@ let CadastroAssistentePage = class CadastroAssistentePage {
     }
 };
 CadastroAssistentePage.ctorParameters = () => [
-    { type: _angular_router__WEBPACK_IMPORTED_MODULE_3__["ActivatedRoute"] },
-    { type: _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["LoadingController"] },
-    { type: src_app_services_assistenteCadastro_assistente_cadastro_service__WEBPACK_IMPORTED_MODULE_5__["AssistenteCadastroService"] },
-    { type: _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["NavController"] },
-    { type: _ionic_native_camera_ngx__WEBPACK_IMPORTED_MODULE_6__["Camera"] },
-    { type: src_app_core_overlay_service__WEBPACK_IMPORTED_MODULE_8__["OverlayService"] },
-    { type: src_app_services_assistenteCadastro_assistenteCadastro__WEBPACK_IMPORTED_MODULE_2__["AssistenteCadastro"] }
+    { type: _angular_router__WEBPACK_IMPORTED_MODULE_2__["ActivatedRoute"] },
+    { type: _ionic_angular__WEBPACK_IMPORTED_MODULE_3__["LoadingController"] },
+    { type: src_app_services_assistenteCadastro_assistente_cadastro_service__WEBPACK_IMPORTED_MODULE_4__["AssistenteCadastroService"] },
+    { type: _ionic_angular__WEBPACK_IMPORTED_MODULE_3__["NavController"] },
+    { type: _ionic_native_camera_ngx__WEBPACK_IMPORTED_MODULE_5__["Camera"] },
+    { type: src_app_core_overlay_service__WEBPACK_IMPORTED_MODULE_7__["OverlayService"] }
 ];
 tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
     Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["ViewChild"])('form', null),
-    tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:type", _angular_forms__WEBPACK_IMPORTED_MODULE_7__["NgForm"])
+    tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:type", _angular_forms__WEBPACK_IMPORTED_MODULE_6__["NgForm"])
 ], CadastroAssistentePage.prototype, "form", void 0);
 CadastroAssistentePage = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
     Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
@@ -279,13 +275,12 @@ CadastroAssistentePage = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
         template: __webpack_require__(/*! raw-loader!./cadastro-assistente.page.html */ "./node_modules/raw-loader/index.js!./src/app/pages/cadastro-assistente/cadastro-assistente.page.html"),
         styles: [__webpack_require__(/*! ./cadastro-assistente.page.scss */ "./src/app/pages/cadastro-assistente/cadastro-assistente.page.scss")]
     }),
-    tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [_angular_router__WEBPACK_IMPORTED_MODULE_3__["ActivatedRoute"],
-        _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["LoadingController"],
-        src_app_services_assistenteCadastro_assistente_cadastro_service__WEBPACK_IMPORTED_MODULE_5__["AssistenteCadastroService"],
-        _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["NavController"],
-        _ionic_native_camera_ngx__WEBPACK_IMPORTED_MODULE_6__["Camera"],
-        src_app_core_overlay_service__WEBPACK_IMPORTED_MODULE_8__["OverlayService"],
-        src_app_services_assistenteCadastro_assistenteCadastro__WEBPACK_IMPORTED_MODULE_2__["AssistenteCadastro"]])
+    tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [_angular_router__WEBPACK_IMPORTED_MODULE_2__["ActivatedRoute"],
+        _ionic_angular__WEBPACK_IMPORTED_MODULE_3__["LoadingController"],
+        src_app_services_assistenteCadastro_assistente_cadastro_service__WEBPACK_IMPORTED_MODULE_4__["AssistenteCadastroService"],
+        _ionic_angular__WEBPACK_IMPORTED_MODULE_3__["NavController"],
+        _ionic_native_camera_ngx__WEBPACK_IMPORTED_MODULE_5__["Camera"],
+        src_app_core_overlay_service__WEBPACK_IMPORTED_MODULE_7__["OverlayService"]])
 ], CadastroAssistentePage);
 
 
@@ -347,22 +342,6 @@ AssistenteCadastroService = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
     tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [angularfire2_firestore__WEBPACK_IMPORTED_MODULE_2__["AngularFirestore"]])
 ], AssistenteCadastroService);
 
-
-
-/***/ }),
-
-/***/ "./src/app/services/assistenteCadastro/assistenteCadastro.ts":
-/*!*******************************************************************!*\
-  !*** ./src/app/services/assistenteCadastro/assistenteCadastro.ts ***!
-  \*******************************************************************/
-/*! exports provided: AssistenteCadastro */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AssistenteCadastro", function() { return AssistenteCadastro; });
-class AssistenteCadastro {
-}
 
 
 /***/ }),
