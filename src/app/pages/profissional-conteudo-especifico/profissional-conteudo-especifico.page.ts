0178bbe654983591f20b4dc8b7d45d4581a8ecd1
Marcos Gronytzki <gronytzki>
2019-11-05 17:31:54 -0200

    enviando mecanimso de listasos textos

--- src/app/pages/profissional-conteudo-especifico/profissional-conteudo-especifico.page.ts
+++ src/app/pages/profissional-conteudo-especifico/profissional-conteudo-especifico.page.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfissionalConteudoTexto } from 'src/app/services/profissionalConteudoTexto/profissionalConteudoTexto';
+import { Subscription } from 'rxjs';
+import { NavController } from '@ionic/angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { ProfissionalConteudoTextoService } from 'src/app/services/profissionalConteudoTexto/profissional-conteudo-texto.service';
+import { QueroConversarService } from 'src/app/services/queroConversar/quero-conversar.service';
 
 @Component({
   selector: 'app-profissional-conteudo-especifico',
   templateUrl: './profissional-conteudo-especifico.page.html',
-  styleUrls: ['./profissional-conteudo-especifico.page.scss'],
+  styleUrls: ['./profissional-conteudo-especifico.page.scss']
 })
 export class ProfissionalConteudoEspecificoPage implements OnInit {
+  idUsuario: string;
 
-  constructor() { }
+  public tituloTextoTela: string;
+  public descricaoTextoTela: string;
+  public autorTextoTela: string;
+
+  public avalicaoQueroConversar: number;
+
+  public profissionalConteudoTexto: ProfissionalConteudoTexto[];
+  public listProfissionalConteudoTexto: Subscription;
+
+  public avaliacaoGlobal: number;
+
+  constructor(
+    private navctrl: NavController,
+    private auth: AngularFireAuth,
+    private profissionalConteudoTextoService: ProfissionalConteudoTextoService,
+    private queroConversarService: QueroConversarService
+  ) {}
+
+  public listQueroConversar: Subscription;
+  public subscription: Subscription;
 
   ngOnInit() {
+    this.idUsuario = this.auth.auth.currentUser.uid;
+    this.buscaDadosTexto(this.idUsuario);
   }
 
+  buscaDadosTexto(idUsuario) {
+    //Pegando os dados informados pelo usuário, sobre a sua situação
+    this.listQueroConversar = this.queroConversarService.getTodo(this.idUsuario).subscribe(res => {
+      this.avalicaoQueroConversar = res.avaliacao;
+      this.avaliacaoGlobal = this.avalicaoQueroConversar;
+
+      // Atribuindo na pesquisa
+      this.profissionalConteudoTextoService
+        .getTodosPoAvaliacao(this.avaliacaoGlobal)
+        .subscribe(res => {
+          this.profissionalConteudoTexto = res;
+
+          // Percorrendo os dados da coleção
+          res.forEach(x => {
+            this.tituloTextoTela = x.tituloTexto;
+            this.descricaoTextoTela = x.texto;
+            this.autorTextoTela = x.autorTexto;
+
+            console.log('Exibindo titulo- >' + this.tituloTextoTela);
+            console.log('Exibindo texto- >' + this.descricaoTextoTela);
+          });
+        });
+    });
+  }
 }
